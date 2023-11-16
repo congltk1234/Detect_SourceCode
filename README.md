255a7214f73bd9a695bb713d86e747f063076813
STC1702 <stc17022002@gmail.com>
2023-11-16 20:54:35 +0700

    Add CodeBERT route

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 
 
-
+`!pip install -r requirements.txt --use-deprecated=legacy-resolver`
 
 
 https://stackoverflow.com/questions/60157335/cant-pip-install-tensorflow-msvcp140-1-dll-missing 
--- main.py
+++ main.py
@@ -17,13 +17,30 @@ class Item(BaseModel):
 async def root():
     return {"message": "Hello World"}
 
-# @app.get("/guesslang")
-# async def test_guesslang():
-#     return {"message": "Guesslang Test"}
+@app.get("/guesslang")
+async def test_guesslang():
+    return {"message": "Guesslang Test"}
 
 ## Guesslang
 @app.post("/guesslang")
-async def language_name(item: Item):
+async def guesslangTest(item: Item):
     name = guess.language_name(item.text)
+    print(name)
+    return name
+
+
+from transformers import TextClassificationPipeline,RobertaTokenizer, RobertaForSequenceClassification
+## CodeBERT
+@app.post("/CodeBERT")
+async def codebertTest(item: Item):
+    CODEBERTA_LANGUAGE_ID = "huggingface/CodeBERTa-language-id"
+    tokenizer = RobertaTokenizer.from_pretrained(CODEBERTA_LANGUAGE_ID)
+    model = RobertaForSequenceClassification.from_pretrained(CODEBERTA_LANGUAGE_ID)
+
+    pipeline = TextClassificationPipeline(
+        model=model,
+        tokenizer=tokenizer
+    )
+    name = pipeline(item.text)[0]
     print(name)
     return name
